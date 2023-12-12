001ef0e1f6705854be02518c067cd06810af6304
Rohit H Solanki <solanki.rohit6105@gmail.com>
2023-12-12 16:21:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 import tkinter.messagebox as tkmsgbox
+import tkinter
 import json
 import os
 import glob
@@ -9,47 +10,58 @@ from settings import SettingsWindow
 from student_choose import StudentsWindow
 from process_all import process_all_files_user
 from PIL import ImageTk
+# from printers import *
 
 root = ctk.CTk() #set root window
 ctk.set_appearance_mode("Light") #theme = light
 # root.attributes("-alpha", 0.9) # transparency
-root.minsize(600, 370) #set mninimum size of window
+root.minsize(650, 370) #set mninimum size of window
 root.title("Line Printer Management System") # title of program
 #logo icon
-root.wm_iconbitmap()
+# root.wm_iconbitmap()
+root.iconbitmap()
 root.iconphoto(False, ImageTk.PhotoImage(file=("logo.ico")))
 # functions
 
 def open_settings():
     settings_window = SettingsWindow(root) #display settings window
     # settings_window.wm_attributes("-topmost", True) # always on top of all windows
-    
+
 #connect to server for z drive
 def connect_server():
     try:
         os.system("net use Z: \\\\" + ip_entry.get())
     except:
-        tkmsgbox.showerror("Z error", "Z dive exists or can't be created")
+        tkmsgbox.showerror("Z error", "Z drive exists or can't be created")
 
 # remover server , z drive
 def disconnect_server():
     os.system("net use Z: /delete")
 
+printer_display_frame = ctk.CTkFrame(root, fg_color="transparent")
+printer_display_frame.pack()
+printer_name_label = ctk.CTkLabel(printer_display_frame, text="none")
+printer_name_label.grid(row=0, column=0)
+printer_on_off = ctk.CTkLabel(printer_display_frame, text="offline")
+printer_on_off.grid(row=0, column=1)
+
 # top frame
-top_frame = ctk.CTkFrame(root, fg_color="#ebebeb") # color will transparent to merge with background
+top_frame = ctk.CTkFrame(root, fg_color="transparent") # color will transparent to merge with background
 top_frame.pack(anchor=ctk.N, padx = 15, pady = 15, fill = ctk.X)
 
 #settings button to open settings as inner seperate window
 settings_btn = ctk.CTkButton(top_frame, text="⏣", font=('', 30), width = 30, command=open_settings)
 settings_btn.pack(side = ctk.RIGHT)
+# settings_btn.grid(row = 0, column = 2, padx=50, sticky=ctk.E)
 
 # today's date
 today = date.today().strftime("%d-%m-%Y") # formatting date
 today_display = ctk.CTkLabel(top_frame, text = today)
 today_display.pack(side = ctk.LEFT)
+# today_display.grid(row = 0, column = 0,padx=50)
 
 # all main inputs in central frame
-central_frame = ctk.CTkFrame(root, fg_color="#ebebeb") # central widget for every entries
+central_frame = ctk.CTkFrame(root, fg_color="transparent") # central widget for every entries
 central_frame.pack(padx=50, pady=50)
 
 # ip
@@ -124,14 +136,24 @@ sub_entry = ctk.CTkComboBox(central_frame, values=[""])
 sub_entry.grid(row = 2, column = 1, pady = 5, padx = 10)
 
 # buttons
-btn_frame = ctk.CTkFrame(root, fg_color="#ebebeb")
+btn_frame = ctk.CTkFrame(root, fg_color="transparent")
 btn_frame.pack(anchor=ctk.SE, padx = 15, pady = 15)
 
 connect_btn = ctk.CTkButton(btn_frame, text="Connect", command=connect_server, width=90)
-connect_btn.grid(row=0, column=0, padx = 15, pady = 15)
+connect_btn.grid(row=0, column=3, padx = 15, pady = 15)
 
 disconnect_btn = ctk.CTkButton(btn_frame, text="Disconnect", command=disconnect_server, width=110)
-disconnect_btn.grid(row=0, column=1, padx = 15, pady = 15)
+disconnect_btn.grid(row=0, column=4, padx = 15, pady = 15)
+
+#printers
+# printer_frame = ctk.CTkFrame(root, fg_color="transparent")
+# printer_frame.pack(anchor=ctk.SW, padx = 15, pady = 15)
+
+printer_btn = ctk.CTkButton(btn_frame, text="Printers", width=80)
+printer_btn.grid(row=0, column=1, padx = 15, pady = 15)
+
+printer_btn = ctk.CTkButton(btn_frame, text="Status", width=80)
+printer_btn.grid(row=0, column=2, padx = 15, pady = 15)
 
 global data_path
 
@@ -140,7 +162,13 @@ def student_selection():
     StudentsWindow(sub_entry.get(), course_entry.get(), data_path)
 
 print_btn = ctk.CTkButton(btn_frame, text="Print", width=80, command=student_selection)
-print_btn.grid(row=0, column=2, padx = 15, pady = 15)
+print_btn.grid(row=0, column=5, padx = 15, pady = 15)
+
+printer_name = tkinter.StringVar(value=None)
+
+def printer_status():
+    pending_frame = ctk.CTkFrame(root)
+    pending_frame.pack()
 
 # inserting ip
 try:
@@ -150,6 +178,9 @@ try:
         ip_entry.insert(0, settings_data['default_ip'])
         data_path = settings_data['default_data_folder']
         kermit_path = settings_data['kermit_path']
+        printer_name.set(settings_data['printer'])
+        printer_name_label.configure(text=f"{printer_name.get()}: ")
+        # print(printer_name.get())
 
         # print(data_path, kermit_path)
 
